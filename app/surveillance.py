86733aee0daf9ae8ba0d57440969be097cb9dad3
skap3214 <kapadiasoami@gmail.com>
2023-04-09 11:41:46 -0400

    using mediapipe

--- app/surveillance.py
+++ app/surveillance.py
@@ -3,10 +3,12 @@ import streamlit as st
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
 import numpy as np
 import cv2
+import mediapipe as mp
 
 st.set_page_config(page_title='Surveillance')
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_profileface.xml')
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
 with st.sidebar:
     st.write('# Settings')
     display_settings = {
@@ -19,35 +21,42 @@ def audio_frame_callback(frame: av.AudioFrame) -> av.AudioFrame:
 
     # sound logic
 
-    result_sound = sound if display_settings['echo'] else np.zeros_like(sound)
-    result_frame = av.AudioFrame.from_ndarray(result_sound, layout=frame.layout.name)
-    result_frame.sample_rate = frame.sample_rate
-    return result_frame
+
+    
+    
+    return av.AudioFrame
+# result_sound = sound if display_settings['echo'] else np.zeros_like(sound)
+# result_frame = av.AudioFrame.from_ndarray(result_sound, layout=frame.layout.name)
+# result_frame.sample_rate = frame.sample_rate
+st.session_state['faces'] = 0
 
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    image = frame.to_ndarray(format='bgr24')
+     # Initialize face detection
+    with mp_face_detection.FaceDetection(min_detection_confidence=0.2) as face_detection:
 
-    to_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces=face_cascade.detectMultiScale(to_gray,1.3,5)
-    num_faces = 0
-    profiles = profile_cascade.detectMultiScale(to_gray,1.3,5)
-    mirrored_image = image[:,::-1,:]
-    mirrored_to_gray = cv2.cvtColor(mirrored_image, cv2.COLOR_BGR2GRAY)
-    profiles_mirrored = profile_cascade.detectMultiScale(mirrored_to_gray,1.3,5)
-    for (x,y,w,h) in profiles_mirrored:
-        cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
-    num_faces+=len(profiles_mirrored)
-    for (x,y,w,h) in profiles:
-        cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
-    num_faces+=len(profiles)
+        # Convert av.VideoFrame to ndarray
+        img_array = np.array(frame.to_ndarray(format='bgr24'))
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
-    mirrored = image[:,::-1,:] if display_settings['mirror'] else image
-    num_faces+=len(faces)
-    print(num_faces)
+        # Convert the image to RGB, as MediaPipe uses RGB
+        img_rgb = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
 
-    return av.VideoFrame.from_ndarray(mirrored, format='bgr24')
+        # Create a mutable MediaPipe image object
+        img_rgb.flags.writeable = True
+
+        # Perform face detection
+        results = face_detection.process(img_rgb)
+
+        # Draw face landmarks on the image
+        img_rgb.flags.writeable = False
+        img_array = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(img_array, detection)
+            st.session_state['faces'] = len(results.detections)
+        # Convert the modified ndarray back to av.VideoFrame
+        new_video_frame = av.VideoFrame.from_ndarray(img_array, format='bgr24')
+
+    return new_video_frame
 
 streamer = webrtc_streamer(
     key='surveillance',
